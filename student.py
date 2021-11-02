6b905e7ab2cead509825c4dd006abae54b9a479e
Jesse Schadick <91563212+schadickj@users.noreply.github.com>
2021-11-02 19:06:10 +0000

    added movement

--- student.py
+++ student.py
@@ -57,7 +57,7 @@ class Piggy(PiggyParent):
     ****************
     '''
     def jesse(self):
-      pass
+      deg_fwd(360)
 
     def dance(self):
         """A higher-ordered algorithm to make your robot dance"""
