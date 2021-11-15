aa472fd27a2b1b9b616033030a4b7e091c525a44
Jesse Schadick <91563212+schadickj@users.noreply.github.com>
2021-11-15 20:03:19 +0000

    box boy

--- student.py
+++ student.py
@@ -63,6 +63,9 @@ class Piggy(PiggyParent):
         if self.read_distance() < 100:
           self.right()
           time.sleep(1)
+          self.forward()
+          time.sleep(1)
+          self.left()
         else:
           pass
   
