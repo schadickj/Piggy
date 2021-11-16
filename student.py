90a6dc91d9734dda43a320f897414dc3fca123d4
Jesse Schadick <91563212+schadickj@users.noreply.github.com>
2021-11-16 19:29:42 +0000

    undo

--- student.py
+++ student.py
@@ -63,15 +63,15 @@ class Piggy(PiggyParent):
         if self.read_distance() < 100:
           self.stop()
           self.servo(1000)
-          self.read_distance(1)
+          self.read_distance()
           time.sleep(1.5)
           self.servo(2000)
-          self.read_distance(2)
+          self.read_distance()
           time.sleep(1.5)
           self.servo(1400)
         else:
           pass
-  #box boy is back for move to box and go around it
+  
     def square(self):
       self.deg_fwd(360)
       time.sleep(3)
