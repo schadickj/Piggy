36adea7bd98511d9e1c8de84a685c29eec09ca0a
Jesse Schadick <91563212+schadickj@users.noreply.github.com>
2021-11-18 19:23:02 +0000

    Its a test

--- student.py
+++ student.py
@@ -69,10 +69,11 @@ class Piggy(PiggyParent):
           left = self.read_distance() 
           time.sleep(1.5)
           self.servo(1400)
-          if right > left:
+          elif right > left:
             self.right()
           elif left > right:
             self.left()
+
         else:
           pass
 
