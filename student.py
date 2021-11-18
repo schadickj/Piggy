f1e106b25a280051c3ccee652072b71b16a74fba
Jesse Schadick <91563212+schadickj@users.noreply.github.com>
2021-11-18 19:25:02 +0000

    (Parthases)

--- student.py
+++ student.py
@@ -69,11 +69,10 @@ class Piggy(PiggyParent):
           left = self.read_distance() 
           time.sleep(1.5)
           self.servo(1400)
-          if right > left:
+          if (right > left):
             self.right()
-          elif left > right:
+          elif (left > right):
             self.left()
-
         else:
           pass
 
