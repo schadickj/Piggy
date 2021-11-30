116620653d737e2c89aca1f92401cb2e7cc9332a
Jesse Schadick <91563212+schadickj@users.noreply.github.com>
2021-11-30 20:16:38 +0000

    Serve Faster

--- student.py
+++ student.py
@@ -62,9 +62,9 @@ class Piggy(PiggyParent):
         self.fwd()
         self.servo(1000)
         if self.read_distance() < 200:
-          time.sleep(.1)
+          time.sleep(.3)
           self.left()
-          time.sleep(.1)
+          time.sleep(.3)
           self.fwd()
         else:
           pass
