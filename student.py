b332f43ed419fe413043e3c4af5db2b199c72851
Jesse Schadick <91563212+schadickj@users.noreply.github.com>
2021-12-02 19:21:29 +0000

    stop

--- student.py
+++ student.py
@@ -62,7 +62,7 @@ class Piggy(PiggyParent):
         self.fwd()
         self.servo(1000)
         if self.read_distance() < 200:
-        
+          self.stop
           time.sleep(.5)
           self.left()
           time.sleep(1)
@@ -74,6 +74,7 @@ class Piggy(PiggyParent):
         time.sleep(.1)
         self.servo(1800)
         if self.read_distance() < 200:
+          self.stop
           time.sleep(.5)
           self.right()
           time.sleep(1)
