607d7f84a176fc17e8985c678ec0655ac0a4d8c5
Jesse Schadick <91563212+schadickj@users.noreply.github.com>
2021-12-07 19:35:42 +0000

    up by 100

--- student.py
+++ student.py
@@ -96,7 +96,7 @@ class Piggy(PiggyParent):
     def close_edge(self):
       while True:
         self.fwd()
-        if self.read_distance() < 100:
+        if self.read_distance() < 200:
           self.stop()
           self.servo(1000)
           right = self.read_distance() 
