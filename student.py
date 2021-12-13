3c8540b50c9fe6f10669c604b474b1d201be47d7
Jesse Schadick <91563212+schadickj@users.noreply.github.com>
2021-12-13 19:48:20 +0000

    added servo

--- student.py
+++ student.py
@@ -92,6 +92,7 @@ class Piggy(PiggyParent):
   
     def maze_solver(self):
       while True:
+        self.servo(1400)
         self.fwd()
         if self.read_distance() < 100:
           self.stop()
